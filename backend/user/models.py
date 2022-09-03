929beeb0c875d340a09fbc8db3eb1ff90e410416
Ray Lee <wooo519@naver.com>
2022-09-03 23:07:43 +0900

    feat : add models username

--- backend/user/models.py
+++ backend/user/models.py
@@ -3,7 +3,7 @@ import uuid
 
 class user(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(unique=True, max_length=20, null=True, blank=True)
+    username = models.CharField(unique=True, max_length=20, null=True, blank=True)
     alias = models.CharField(unique=True, max_length=20)
     # password = models.CharField(unique=True, max_length=20)
     password = models.BinaryField(max_length=60)
