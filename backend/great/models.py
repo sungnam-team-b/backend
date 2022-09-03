bc6a72be2ded20cd130d78a55b6e90438351a861
Jung Yunho <yunhobb98@gmail.com>
2022-09-04 01:51:18 +0900

    feat: add great models

--- backend/great/models.py
+++ backend/great/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from user.models import user
 
-# Create your models here.
+
+class picture(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(user, on_delete=models.CASCADE, db_column = 'user_id')
+    picture_no = models.IntegerField(default=0)
+    picture_url = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True)
+
+    class Meta:
+        db_table = 'picture'
+
+class great(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=200)
+    img_url = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True)
+
+    class Meta:
+        db_table = 'great'
+
+class result(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(user, on_delete=models.CASCADE, db_column = 'user_id')
+    picture_id = models.ForeignKey(picture, on_delete=models.CASCADE, db_colume ='picture_id')
+    great_id = models.ForeignKey(great, on_delete=models.CASCADE, db_colume ='great_id')
+    similarity = models.FloatField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True)
+
+    class Meta:
+        db_table = 'result'
+
