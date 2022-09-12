f357bb4e8cde381cfd5d85410f8c30e7e8f73e7c
Jung Yunho <yunhobb98@gmail.com>
2022-09-12 20:33:08 +0900

    feat: edit user entity

--- backend/user/models.py
+++ backend/user/models.py
@@ -2,13 +2,16 @@ from django.db import models
 import uuid 
 
 class user(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.AutoField(primary_key=True)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(unique=True, max_length=20, null=True, blank=True)
     alias = models.CharField(unique=True, max_length=20)
-    # password = models.CharField(unique=True, max_length=20)
     password = models.BinaryField(max_length=60)
     salt = models.BinaryField(max_length=29)
     email = models.CharField(max_length=50)
     active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'member'
