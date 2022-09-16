fab452c8e3dbf9b2053b2d58287e43efa15015d5
Ray Lee <wooo519@naver.com>
2022-09-17 00:52:23 +0900

    feat : set models

--- backend/user/models.py
+++ backend/user/models.py
@@ -3,11 +3,11 @@ import uuid
 
 class user(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    username = models.CharField(unique=True, max_length=20, null=True, blank=True)
-    alias = models.CharField(unique=True, max_length=20)
+    username = models.CharField(unique=True, max_length=200, null=True, blank=True)
+    alias = models.CharField(unique=True, max_length=200)
     password = models.BinaryField(max_length=60)
-    salt = models.BinaryField(max_length=29)
-    email = models.CharField(max_length=50)
+    salt = models.BinaryField(max_length=30)
+    email = models.CharField(max_length=200)
     active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
