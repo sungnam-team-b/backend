8d18978912ba4e8ab3cc882c99a01abca965c04a
Jung Yunho <yunhobb98@gmail.com>
2022-09-17 00:53:00 +0900

    feat: add test api for add user table

--- backend/great/urls.py
+++ backend/great/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.greatview), # 기본
     path('list', views.list), #greatlist
     path('greatlist', views.get_greatlist), #great모든 목록 조회
-    path('ai',views.airesult)
+    path('ai',views.airesult),
+    path('models',views.addmodel)
 ]
