0873bc550f5126eba7b38607574270a2d5cc1f40
Hanjione <gkswldnj0803@gmail.com>
2022-09-17 22:08:25 +0900

    test: api response success when userId not exist

--- backend/great/views.py
+++ backend/great/views.py
@@ -5,6 +5,7 @@ from .models import Great,Result
 from .serializers import GreatlistResponse, MyPageResponse
 from rest_framework.response import Response
 from django.core.cache import cache
+import requests
 
 def greatview(request):
     return JsonResponse({"id" : "test"})
@@ -20,13 +21,25 @@ def get_greatlist(request):
     serializer = GreatlistResponse(greatlist, many=True)
     return Response(serializer.data)
 
+# class mypage(View):
+#     def get(self, request, product_id):						
+#         if not Product.objects.filter(id=product_id).exists():
+#             return JsonResponse({'message': 'PRODUCT_DOES_NOT_EXIST'}, status=404)
+
+#         product = Product.objects.get(id=product_id)
 #마이페이지 
 @api_view(['GET'])
-def mypage(request,userId):
+def mypage(request, userId):
     print('userId:'+ str(userId) )
+    if not Result.objects.filter(user=userId).exists():
+        return JsonResponse({userId: 'PRODUCT_DOES_NOT_EXIST'}, status=404)
     resultByUser = Result.objects.filter(user=userId)
     serializer = MyPageResponse(resultByUser, many=True)
     return Response(serializer.data)
+    
+    
+
+
     # if cache.get("logindata"):
     #     logindata = cache.get("logindata")
 
