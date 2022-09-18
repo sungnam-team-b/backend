1b5475e399612820c7d77c6257231aff8a110f0a
Ray Lee <wooo519@naver.com>
2022-09-18 21:09:00 +0900

    chore : for merge

--- backend/user/userUtil.py
+++ backend/user/userUtil.py
@@ -35,6 +35,25 @@ def user_ispassword(password, user_data):
     hash_password = bcrypt.hashpw(password, user_data.salt)
     return hash_password == user_data.password
 
+class UserDuplicateCheck:
+    @staticmethod
+    def alias(alias):
+        if user_find_alias(alias):
+            return False
+        return True
+
+    @staticmethod
+    def email(email):
+        if user_find_email(email):
+            return False
+        return True
+
+    @staticmethod
+    def username(username):
+        if user_find_name(username):
+            return False
+        return True
+
 #tokens
 def user_get_access_token(user_data):
     return jwt.encode(
@@ -47,3 +66,26 @@ def user_get_refresh_token(user_data):
     return jwt.encode({'id': str(user_data.id), 'alias': user_data.alias, 'exp': datetime.utcnow() + timedelta(days=7),
                        'type': "refresh_token"},
                       JWT_SECRET_KEY, ALGORITHM).decode('utf-8')
+
+def user_token_to_data(token):
+    try:
+        payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=ALGORITHM)
+    except jwt.exceptions.ExpiredSignatureError: #토큰이 날짜가 만료되었을 때
+        return "Expired_Token"
+    except jwt.exceptions.DecodeError: # 토큰 디코딩 오류 생겼을 때
+        return "Invalid_Token"
+    return payload
+
+
+def user_refresh_get_access(refresh_token):
+    try:
+        payload = jwt.decode(refresh_token, JWT_SECRET_KEY, algorithms=ALGORITHM)
+        access_token = jwt.encode(
+            {'id': payload.get('id'), 'username': payload.get('username'), 'alias': payload.get('alias'),
+             'email': payload.get('email'), 'type': "access_token",
+             'exp': datetime.utcnow() + timedelta(minutes=5)}, JWT_SECRET_KEY, ALGORITHM).decode('utf-8')
+    except jwt.exceptions.ExpiredSignatureError or jwt.exceptions.DecodeError:
+        return False
+    return access_token
+    
+    
