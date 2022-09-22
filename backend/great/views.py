ffcd4cf3f7ad21a45afa63f5bcb4c0d9cf0c1174
Jung Yunho <a01049048063@gmail.com>
2022-09-22 22:53:26 +0900

    feat: fix range error

--- backend/great/views.py
+++ backend/great/views.py
@@ -45,79 +45,6 @@ def get_greatlist(request):
     serializer = GreatlistResponse(greatlist, many=True)
     return Response(serializer.data)
 
-# @api_view(['POST'])
-# def airesult(request):
-#     #이미지 로컬에 저장
-#     picuuid = str(uuid4())
-#     file = request.FILES['filename']
-#     useruuid = request.POST['user_id'] #uuid로 모델에서 값 조회하는걸로 변경
-#     userquery = user.objects.filter(uuid = useruuid).values()
-#     userid = (userquery[0])['id']
-#     fs = FileSystemStorage(location='media', base_url='media')
-#     filename = fs.save(picuuid+'.png', file)
-#     uploaded_file_url = fs.url(filename)
-#     #s3 버킷에 업로드
-#     s3 = s3_connection()
-#     retPut = s3_put_object(
-#         s3, AWS_STORAGE_BUCKET_NAME, '/app/media/' + str(picuuid) + '.png', 'image/' + str(picuuid) + '.png')
-#     retGet = s3_get_image_url(s3, 'image/' + str(picuuid) + '.png') #s3 이미지 url
-#     #picture 정보 db에 저장
-#     Picture.objects.create(user_id = user.objects.get(id=userid), picture_url = retGet, uuid = picuuid )
-#     #모델에 사진 넣기
-#     ##################################################################################################################
-#     result = get_ai_result(f'{picuuid}.png')
-#     keys = list(result.keys())
-#     #picture_id 가져오기
-#     pictureid = Picture.objects.filter(uuid = picuuid).values('id')
-#     #ai 결과 db에 저장
-#     result1 = keys[0] # 첫번쨰 key값
-#     result2 = keys[1] # 두번째 key값
-#     result3 = keys[2] # 세번째 key값
-#     a = get_animal_num('abc')
-#     print('#########')
-#     print(int((pictureid[0])['id']))
-#     print(a)
-#     print('#########')
-#     data_convert = {k:float(v) for k,v in result.items()}
-    
-#     ########
-#     # Result.objects.create(user_id = user.objects.get(id = int(userid)),\
-#     #     great_id = Great.objects.get(id = ((get_animal_num(str(result1)))[0])['id']),\
-#     #         picture_id = Picture.objects.get(id = int((pictureid[0])['id'])),\
-#     #             similarity = float(result[f'{str(result1)}']))
-#     # Result.objects.create(user_id = user.objects.get(id = int(userid)),\
-#     #     great_id = Great.objects.get(id = ((get_animal_num(str(result2)))[0])['id']),\
-#     #         picture_id = Picture.objects.get(id = int((pictureid[0])['id'])),\
-#     #             similarity = float(result[f'{str(result2)}']))
-#     # Result.objects.create(user_id = user.objects.get(id = int(userid)),\
-#     #     great_id = Great.objects.get(id = ((get_animal_num(str(result3)))[0])['id']),\
-#     #         picture_id = Picture.objects.get(id = int((pictureid[0])['id'])),\
-#     #             similarity = float(result[f'{str(result3)}']))
-#     ################################
-#     # Result.objects.create(user_id = user.objects.get(id = int(userid)),\
-#     #     great_id = Great.objects.get(id = 123),\
-#     #         picture_id = Picture.objects.get(id = int((pictureid[0])['id'])),\
-#     #             similarity = float(result[f'{str(result1)}']))
-#     ################################
-#     Result.objects.create(user_id = user.objects.get(id = int(userid)),\
-#         great_id = Great.objects.get(id = ((get_animal_num('abc'))[0])['id']),\
-#             picture_id = Picture.objects.get(id = int((pictureid[0])['id'])),\
-#                 similarity = float(result[f'{str(result1)}']))            
-#     Result.objects.create(user_id = user.objects.get(id = int(userid)),\
-#         great_id = Great.objects.get(id = ((get_animal_num('BTS'))[0])['id']),\
-#             picture_id = Picture.objects.get(id = int((pictureid[0])['id'])),\
-#                 similarity = float(result[f'{str(result2)}']))
-#     Result.objects.create(user_id = user.objects.get(id = int(userid)),\
-#         great_id = Great.objects.get(id = ((get_animal_num('name'))[0])['id']),\
-#             picture_id = Picture.objects.get(id = int((pictureid[0])['id'])),\
-#                 similarity = float(result[f'{str(result3)}']))
-#     returnresult = {}
-#     returnresult['result'] = data_convert
-#     returnresult['userimage'] = retGet
-#     returnresult['animalimage'] = s3_get_image_url(s3, 'animal/' + str(result1) + '.png')
-#     os.remove(f'/app/media/{picuuid}.png')
-#     return JsonResponse(returnresult, status = 201)
-
 @api_view(['POST'])
 def get_task_id(request,user_id):
     picuuid = str(uuid4())
@@ -223,6 +150,7 @@ def get_task_result(request, user_id, task_id):
     returnresult['animalimage'] = s3_get_image_url(s3, 'animal/' + str(result1) + '.png')
     print(returnresult)
     os.remove(f'/app/media/{picuuid}.png')
+
     return JsonResponse(returnresult, status = 201)
     
 
@@ -239,11 +167,12 @@ def ranking(request):
         .values_list('great_id','user_id','similarity')
     x = list(x[0:10])
     a = Great.objects.get(id = x[0][0]).name
-    for i in range(0,10):
+    # for i in range(0,10):
+    for i in range(0,len(x)):
         alist.append(Great.objects.get(id = x[i][0]).name) #alias
         blist.append(user.objects.get(id = x[i][1]).alias) #animalname
         clist.append(x[i][2]) #similarity
-    for i in range(10):
+    for i in range(len(x)):
         returnrank[i] = {}
         returnrank[i]['alias'] = alist[i]
         returnrank[i]['name'] = blist[i]
