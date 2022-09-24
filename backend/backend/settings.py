e00dffdebf2d6e83a6071eb954a87031eea34548
Jung Yunho <a01049048063@gmail.com>
2022-09-24 21:27:11 +0900

    feat: add python metrics collector

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,10 +43,13 @@ INSTALLED_APPS = [
     'django_celery_results',
     # apps
     'user',
-    'great'
+    'great',
+    # monitoring
+    'django_prometheus'
 ]
 
 MIDDLEWARE = [
+    'django_prometheus.middleware.PrometheusBeforeMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django_prometheus.middleware.PrometheusAfterMiddleware',
 ]
 
 CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000', 'http://localhost:3000','http://localhost']
